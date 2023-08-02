5e06f755ba4d7b15a0bb8893196aafbe8e250ab6
abekah.ekow <abekah.ekow@gmail.com>
2023-08-02 16:30:10 +0000

    fixing img error in navbar and landing

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 export default function Landing() {
     return (
       <div className="relative overflow-hidden  bg-white">
@@ -8,7 +9,7 @@ export default function Landing() {
                 Summer styles are finally here
               </h1>
               <p className="mt-4 text-xl text-gray-500">
-                This year, our new summer collection will shelter you from the harsh elements of a world that doesn&apos;t care
+                This year, our new summer collection will shelter you from the harsh elements of a world that doesnt care
                 if you live or die.
               </p>
             </div>
@@ -23,14 +24,14 @@ export default function Landing() {
                     <div className="flex items-center space-x-6 lg:space-x-8 mt-32">
                       <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                         <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
-                          <img
+                          <Image
                             src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
                             alt=""
                             className="h-full w-full object-cover object-center"
                           />
                         </div>
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img
+                          <Image
                             src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-02.jpg"
                             alt=""
                             className="h-full w-full object-cover object-center"
@@ -39,21 +40,21 @@ export default function Landing() {
                       </div>
                       <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img
+                          <Image
                             src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-03.jpg"
                             alt=""
                             className="h-full w-full object-cover object-center"
                           />
                         </div>
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img
+                          <Image
                             src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-04.jpg"
                             alt=""
                             className="h-full w-full object-cover object-center"
                           />
                         </div>
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img
+                          <Image
                             src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-05.jpg"
                             alt=""
                             className="h-full w-full object-cover object-center"
@@ -62,14 +63,14 @@ export default function Landing() {
                       </div>
                       <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img
+                          <Image
                             src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-06.jpg"
                             alt=""
                             className="h-full w-full object-cover object-center"
                           />
                         </div>
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img
+                          <Image
                             src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-07.jpg"
                             alt=""
                             className="h-full w-full object-cover object-center"
