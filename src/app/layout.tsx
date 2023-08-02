793a3ee798675e56e6792f1203c8d8db00dc1045
abekah.ekow <abekah.ekow@gmail.com>
2023-08-02 16:14:48 +0000

    added navbar, footer, hero , product, about us and contact us

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body className={inter.className}>
+        <header>
+        <Navbar />
+        </header>
+        
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
